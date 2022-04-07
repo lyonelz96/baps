a43faf7aa3a3c1ac8a060deb32dff3be7788f97b
Lyonel Zamora <lyonelz516@gmail.com>
2022-04-07 13:37:20 -0400

    Add sign_out route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#login'
 
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/sign_out', to: 'sessions#destroy'
+
   get '/user/:uid', to: 'landing_page#dashboard', as: 'dashboard'
 end
