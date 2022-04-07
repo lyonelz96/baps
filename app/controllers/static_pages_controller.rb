f15219959f06438228ed64b737e1a0fe264da220
Lyonel Zamora <lyonelz516@gmail.com>
2022-04-07 15:46:07 -0400

    Add user_signed_in? controller filter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,11 @@
 class StaticPagesController < ApplicationController
+  before_action :user_signed_in?
+
   def login; end
+
+  private
+
+  def user_signed_in?
+    redirect_to dashboard_path if session[:user]
+  end
 end
