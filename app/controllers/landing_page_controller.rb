f0ba39baedd99bc2bae8546eaced1ab05d4b5aea
Lyonel Zamora <lyonelz516@gmail.com>
2022-04-07 15:45:49 -0400

    Add require_login controller filter

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,3 +1,11 @@
 class LandingPageController < ApplicationController
+  before_action :require_login
+
   def dashboard; end
+
+  private
+
+  def require_login
+    redirect_to root_path if session[:user].nil?
+  end
 end
