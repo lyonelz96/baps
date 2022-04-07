715d9bbe589b6c675f4f5cbaa90b3ebd9526ebdb
Lyonel Zamora <lyonelz516@gmail.com>
2022-04-07 13:30:44 -0400

    Add sessions#destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,9 @@ class SessionsController < ApplicationController
     session[:user] = request.env['omniauth.auth'].except(:extra)
     redirect_to dashboard_path(session[:user][:uid])
   end
+
+  def destroy
+    session.delete(:user)
+    redirect_to root_path
+  end
 end
