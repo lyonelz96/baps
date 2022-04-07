11784a1f360f99c751e9126a5ee22c38265ee36b
Lyonel Zamora <lyonelz516@gmail.com>
2022-04-07 13:28:35 -0400

    Add /user/:uid route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#login'
 
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/user/:uid', to: 'landing_page#dashboard', as: 'dashboard'
 end
