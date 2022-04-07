bdc6baff02a9aafb92f216e989fa2492e1fc48d7
Lyonel Zamora <lyonelz516@gmail.com>
2022-04-07 13:28:17 -0400

    Add redirect to dashboard in sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
     session[:user] = request.env['omniauth.auth'].except(:extra)
+    redirect_to dashboard_path(session[:user][:uid])
   end
 end
