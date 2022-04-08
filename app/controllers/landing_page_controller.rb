474465a2b4b3f75cce888064ee22626f810f4944
Lyonel Zamora <lyonelz516@gmail.com>
2022-04-08 19:15:45 -0400

    Implement refresh_token method

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,10 +1,10 @@
 require 'http'
 
 class LandingPageController < ApplicationController
-  before_action :require_login
+  before_action :require_login, :refresh_token
 
   def dashboard
-    render json: followed_artists
+    @user = current_user
   end
 
   private
@@ -13,15 +13,27 @@ class LandingPageController < ApplicationController
     redirect_to root_path if current_user.nil?
   end
 
+  def refresh_token
+    return unless token_expired?
+
+    response = http_refresh_token_json_response
+
+    current_user['credentials']['token'] = response['access_token']
+    current_user['credentials']['expires_at'] = (Time.now + response['expires_in']).to_i
+  end
+
+  def token_expired?
+    Time.now.to_i > current_user['credentials']['expires_at']
+  end
+
   def followed_artists
     url = 'https://api.spotify.com/v1/me/following'
-    token = current_user['credentials']['token']
 
     artists = []
     next_url = nil
 
     loop do
-      response = http_artists_json_response(token, next_url || url)
+      response = http_artists_json_response(next_url || url)
 
       artists += response['artists']['items']
       next_url = response['artists']['next']
@@ -30,10 +42,24 @@ class LandingPageController < ApplicationController
     end
   end
 
-  def http_artists_json_response(token, url)
+  def http_artists_json_response(url)
+    token = current_user['credentials']['token']
+
     JSON.parse HTTP
       .headers('Content-Type' => 'application/json')
       .auth("Bearer #{token}")
       .get(url, params: { type: 'artist', limit: 50 })
+  end
+
+  def http_refresh_token_json_response
+    url = 'https://accounts.spotify.com/api/token'
+    base64_encoded_auth = Base64.strict_encode64("#{Figaro.env.spotify_client_id}:#{Figaro.env.spotify_client_secret}")
+    headers = { 'Content-Type' => 'application/x-www-form-urlencoded',
+                'Authorization' => "Basic #{base64_encoded_auth}" }
+    params = { 'grant_type' => 'refresh_token', 'refresh_token' => current_user['credentials']['refresh_token'] }
+
+    JSON.parse HTTP
+      .headers(headers)
+      .post(url, params: params)
   end
 end
